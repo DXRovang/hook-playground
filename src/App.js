83f7ff98ada51a1b32f270b6955277e4c9da4a19
Alexander Rovang <DXRovang@gmail.com>
2022-04-30 15:42:21 -0400

    adds constants

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import './App.css';
 
 function App() {
 
+  const namesArray = ["John", "Amy", "Ted", "Jane", "Mark", "Sue"]
+  const numbersArray = [3, 1, 6, 44, 89, 101, 2, 897]
+
+  const myObject = {
+    1: "orange", 
+    2: "pear", 
+    3: "banana", 
+    4: "apple",
+    5: "grape"
+  }
+
+  
+
 
   return (
     <>
     <div className="border">
-      <div className="title">Title</div>
-      <div>Body</div>
-      <button>Click</button>
+      <div className="title">My Constants</div>
+      <div>Names: {namesArray.map(name=>name + " - ")}</div>
+      <div>Numbers: {numbersArray.map(number=> number + " - ")}</div>
+      <div>Object: {Object.keys(myObject).map((item, index)=>index + ". " + myObject[item] + " ")}</div>
     </div>
 
     <div className="border">
